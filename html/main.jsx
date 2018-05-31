f8b7c6bf884db43c95ba3f2e6dd5ced3a73d47fc
timeturnback <time.turnback@gmail.com>
2018-05-31 09:54:00 +0700

    login page

---
+++ html/main.jsx
@@ -0,0 +1,45 @@
+class Signin extends React.Component {
+  constructor(props) {
+    super(props);
+    this.signIn = this.signIn.bind(this);
+    this.handleEmailChange = this.handleEmailChange.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    this.state = {
+      email:'',
+      password:''
+    };
+  }
+
+  handleEmailChange(e){
+    this.setState({email:e.target.value})
+  }
+
+  handlePasswordChange(e){
+    this.setState({password:e.target.value})
+  }
+
+  signIn(){
+    alert('Email address is ' + this.state.email + ' Password is ' + this.state.password);
+  }
+
+  render() {
+    return (
+      <form className="form-signin">
+      <h2 className="form-signin-heading"> Please sign in </h2>
+      <label for="inputEmail" className="sr-only"> Email address
+      </label>
+      <input type="email" onChange={this.handleEmailChange} id="inputEmail" className="form-control" placeholder="Email address" required autofocus />
+      <label for="inputPassword" className="sr-only"> Password</label>
+      <input type="password" onChange={this.handlePasswordChange} id="inputPassword" className="form-control" placeholder="Password" required />
+      <button className="btn btn-lg btn-primary btn-block" onClick={this.signIn} type="button"> Sign in
+      </button>
+      </form>
+      )
+  }
+}
+
+ReactDOM.render( <Signin /> ,
+  document.getElementById('app')
+  );
+
+console.log("test");
